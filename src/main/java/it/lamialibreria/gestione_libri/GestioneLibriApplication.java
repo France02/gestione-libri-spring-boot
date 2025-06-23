9f464a0361a0d2d2234cbd5939ff7d337d06dd21
Francesco_Golino <133333709+France02@users.noreply.github.com>
2025-06-23 16:37:14 +0200

    Initial commit: progetto base da Spring Initializr

---
+++ src/main/java/it/lamialibreria/gestione_libri/GestioneLibriApplication.java
@@ -0,0 +1,13 @@
+package it.lamialibreria.gestione_libri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneLibriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneLibriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/lamialibreria/gestione_libri/GestioneLibriApplicationTests.java
@@ -0,0 +1,13 @@
+package it.lamialibreria.gestione_libri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneLibriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
